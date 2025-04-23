b30e2916926d986fc22eec26afa7fb5a664194d5
Leonel Donati <leonelroman321@gmail.com>
2025-04-23 18:32:44 -0300

    implemented in cart page some changes

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,18 +2,20 @@ import SendOrderButton from "@components/ui/send-order";
 import CartCounter from "@components/ui/cart-counter";
 import CartFeed from "@components/ui/cart-feed";
 import DeleteCartButton from "@components/ui/delete-cart-button";
+import AddIce from "@components/ui/add-ice";
+import Counter from "@components/ui/counter";
 
 export default function Page() {
   return (
-    <section className="flex flex-col gap-5 w-full">
+    <section className="flex flex-col items-center gap-5 w-full">
       <div className="flex  w-full justify-center gap-5 items-center flex-col md:flex-row bg-brand-1 p-4 rounded-md">
         <CartCounter />
-
+        <Counter />
         <DeleteCartButton />
 
         <SendOrderButton />
       </div>
-
+      <AddIce />
       <CartFeed />
     </section>
   );
