58d3fb101569f88e1f3e7b57fcae27b65b358ebb
Leonel Donati <leonelroman321@gmail.com>
2025-04-25 20:14:41 -0300

    fixed flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ import bottle from "@assets/bottle.avif";
 import bottleLeft from "@assets/bottle-left.avif";
 export default function Home() {
   return (
-    <section className="flex flex-col flex-grow justify-center items-center gap-5">
+    <section className="flex flex-col flex-grow justify-center items-center gap-5 overflow-hidden">
       <div className="md:max-w-[750px] w-full max-w-[300px]">
         <Logo />
 
         <LogoDesktop />
       </div>
 
-      <div className="flex items-center justify-center gap-4 w-full max-w-sm relative">
+      <div className="flex  items-center justify-center gap-4  relative">
         <Image
           src={bottle}
           alt=""
@@ -30,7 +30,7 @@ export default function Home() {
             className=" animate-shake animate-duration-[3500ms] w-full absolute h-full"
           />
 
-          <div className="px-4 py-2 bg-brand-1 animate-flip-up animate-duration-[3500ms] animate-ease">
+          <div className="px-4 py-2 bg-brand-1 animate-flip-up animate-duration-[3500ms] animate-ease flex">
             VER COMBOS
           </div>
         </Link>
@@ -49,7 +49,7 @@ export default function Home() {
             alt=""
             className="animate-shake animate-duration-[3500ms] w-full absolute h-full"
           />
-          <div className="px-4 py-2 bg-brand-1 animate-flip-up animate-duration-[3500ms] animate-ease">
+          <div className="px-4 py-2 bg-brand-1 animate-flip-up animate-duration-[3500ms] animate-ease flex">
             VER PRODUCTOS
           </div>
         </Link>
